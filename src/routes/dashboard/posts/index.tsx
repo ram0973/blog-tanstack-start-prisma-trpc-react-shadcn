03da0c6270bd6dabaf8d1f8e649002f08aea9dec
ram0973 <ram0973@gmail.com>
2025-06-08 18:24:24 +0300

    next:

--- src/routes/dashboard/posts/index.tsx
+++ src/routes/dashboard/posts/index.tsx
@@ -4,7 +4,7 @@ import { createFileRoute } from "@tanstack/react-router";
 import { columns } from "./-columns";
 //import createServerHelpers,  from '@/lib/trpc/root-provider'
 import { useTRPC } from "@/integrations/trpc/react";
-import { useQuery } from "@tanstack/react-query";
+import { queryOptions, useQuery, useSuspenseQuery } from "@tanstack/react-query";
 import { createServerFn } from "@tanstack/react-start";
 import {prisma} from '@/lib/prisma'
 
@@ -20,23 +20,31 @@ export const fetchAllPosts = createServerFn({ method: 'GET' }).handler(
   },
 );
 
+export const postsQueryOptions = () =>
+  queryOptions({
+    queryKey: ['posts'],
+    queryFn: () => fetchAllPosts(),
+  })
+
 
 export const Route = createFileRoute("/dashboard/posts/")({
   //errorComponent: () => "Oh crap!",
   pendingComponent: Spinner,
   component: DashboardPostsComponent,
+  loader: async ({ context }) => {
+    await context.queryClient.ensureQueryData(postsQueryOptions())
+  },
 });
 
 function DashboardPostsComponent() {
   //const trpc = useTRPC()
   //const { data: posts } = useQuery(trpc.posts.getAll.queryOptions())
-  const { data, isLoading } = useQuery({
-    queryKey: ['posts'],
-    queryFn: () => fetchAllPosts(),
-  });
-  console.log(data)
-
-
+  // const { data, isLoading } = useQuery({
+  //   queryKey: ['posts'],
+  //   queryFn: () => fetchAllPosts(),
+  // });
+  const postsQuery = useSuspenseQuery(postsQueryOptions())
+  const {data} = postsQuery
   return (
     <>
       <DataTable data={data?.posts ?? []} columns={columns} findByField="title"/>
