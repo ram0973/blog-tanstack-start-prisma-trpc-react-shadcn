e1913207603752535487943aa308fbf7427c6f15
ram0973 <ram0973@gmail.com>
2025-06-08 18:42:58 +0300

    next

--- src/routes/dashboard/users/index.tsx
+++ src/routes/dashboard/users/index.tsx
@@ -3,12 +3,35 @@ import { Spinner } from "@/components/spinner";
 import { createFileRoute } from "@tanstack/react-router";
 import { columns } from "./-columns";
 import { useTRPC } from "@/integrations/trpc/react";
-import { useQuery } from "@tanstack/react-query";
+import { queryOptions, useQuery, useSuspenseQuery } from "@tanstack/react-query";
+import { createServerFn } from "@tanstack/react-start";
+import { prisma } from "@/lib/prisma";
+
+export const fetchAllUsers = createServerFn({ method: 'GET' }).handler(
+  async () => {
+    const users = await prisma.user.findMany({
+      orderBy: {
+        createdAt: 'desc',
+      },
+    });
+    const total = await prisma.user.count();
+    return { users, total };
+  },
+);
+
+export const usersQueryOptions = () =>
+  queryOptions({
+    queryKey: ['users'],
+    queryFn: () => fetchAllUsers(),
+  })
 
 
 export const Route = createFileRoute("/dashboard/users/")({
   //errorComponent: () => "Oh crap!",
   pendingComponent: Spinner,
+  loader: async ({ context }) => {
+    await context.queryClient.ensureQueryData(usersQueryOptions())
+  },
   // loader: async ({ context }) => {
   //   // Получаем trpc-клиент из контекста маршрутизатора
   //   const trpc = context.trpc
@@ -20,14 +43,17 @@ export const Route = createFileRoute("/dashboard/users/")({
   component: DashboardPostsComponent,
 });
 
-function DashboardPostsComponent() {
-  const trpc = useTRPC()
-  //const users = Route.useLoaderData()
-  const { data: users } = useQuery(trpc.users.getAll.queryOptions())
 
+
+function DashboardPostsComponent() {
+  //const trpc = useTRPC()
+  //const users = Route.useLoaderData()
+  //const { data: users } = useQuery(trpc.users.getAll.queryOptions())
+  const {data} = useSuspenseQuery(usersQueryOptions())
+  
   return (
     <>
-      <DataTable data={users ?? []} columns={columns}  findByField="name"/>
+      <DataTable data={data.users ?? []} columns={columns}  findByField="name"/>
     </>
   );
 }
